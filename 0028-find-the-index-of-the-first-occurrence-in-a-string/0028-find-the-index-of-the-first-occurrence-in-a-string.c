9a7a6bfa279a4e13819325c9cb82caab1846e2a2
Vasily Shcherbinin <vasily.shcherbinin@outlook.com>
2023-10-11 00:59:53 +0100

    Time: 3 ms (40.89%), Space: 6.3 MB (33.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -1,10 +1,12 @@
 int strStr(char * haystack, char * needle) {
   char* idx = strstr(haystack,needle);
-  if (idx) {
+  if (idx) 
+  {
     printf("%d", idx - haystack);
     return idx - haystack;
-  }
-  else {
+  } 
+  else 
+  {
     return -1;
   }
 }
