0ab8f0cb03eee82a953b3ca5b6efd1db463f7654
Vasily Shcherbinin <vasily.shcherbinin@outlook.com>
2023-10-11 00:58:21 +0100

    Time: 3 ms (40.89%), Space: 6.6 MB (13.77%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -3,10 +3,7 @@ int strStr(char * haystack, char * needle) {
   if (idx) {
     printf("%d", idx - haystack);
     return idx - haystack;
-  } 
-  else {
-    return -1;
   }
-  // Would put this in an else, but having this outside of else is faster.
-  // return -1;
+  // Would put this in an else, but having this outside of else is
+  return -1;
 }
