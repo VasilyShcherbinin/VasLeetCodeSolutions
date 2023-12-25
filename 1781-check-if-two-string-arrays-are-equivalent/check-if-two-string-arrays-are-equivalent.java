219dc5eb738989c93c571255d577a670e74ffe6d
Vasily Shcherbinin <vasily.shcherbinin@outlook.com>
2023-12-25 14:55:33 +0000

    Update check-if-two-string-arrays-are-equivalent.java

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        String string1 = "";
-        String string2 = "";
+        StringBuilder string1 = new StringBuilder();
+        StringBuilder string2 = new StringBuilder();
+        
         for (String word : word1) {
-          string1 += word;
+            string1.append(word);
         }
+
         for (String word : word2) {
-          string2 += word;
+            string2.append(word);
         }
-        if (string1.equals(string2)) {
-          return true;
-        }
-        return false;
+
+        return string1.toString().equals(string2.toString());
     }
 }
