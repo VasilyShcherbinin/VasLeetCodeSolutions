1d42c8d629e544c4b3e56edf75894ce6532e8394
Vasily Shcherbinin <vasily.shcherbinin@outlook.com>
2024-03-03 02:18:09 +0000

    Time: 4 ms (77.17%), Space: 2.2 MB (71.74%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.rs
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.rs
@@ -1,8 +1,10 @@
 impl Solution {
     pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
-        let mut ans = vec![0; nums.len()];
+        let mut ans: Vec<i32> = Vec::with_capacity(nums.len());
+        //let mut ans = vec![0; nums.len()];
         for i in 0..nums.len() {
-            ans[i] = nums[nums[i] as usize];
+            ans.push(nums[nums[i] as usize]);
+            //ans[i] = nums[nums[i] as usize];
         }
         return ans;
     }
