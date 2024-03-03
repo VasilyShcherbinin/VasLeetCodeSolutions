17c506d9c825f76744b9bb530398fb3286f73f4a
Vasily Shcherbinin <vasily.shcherbinin@outlook.com>
2024-03-03 02:24:04 +0000

    Time: 0 ms (100%), Space: 42.1 MB (7.63%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,10 +1,21 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double[] ans = new double[2];
-        double kelvin = celsius + 273.15;
-        double fahrenheit = celsius * 1.80 + 32.00;
-        ans[0] = kelvin;
-        ans[1] = fahrenheit;
-        return ans;
+        final double KELVIN_OFFSET = 273.15;
+        final double FAHRENHEIT_FACTOR = 1.8;
+        final double FAHRENHEIT_OFFSET = 32.0;
+        
+        double[] result = new double[2];
+        
+        // Convert Celsius to Kelvin
+        double kelvin = celsius + KELVIN_OFFSET;
+        
+        // Convert Celsius to Fahrenheit
+        double fahrenheit = celsius * FAHRENHEIT_FACTOR + FAHRENHEIT_OFFSET;
+        
+        // Populate result array
+        result[0] = kelvin;
+        result[1] = fahrenheit;
+        
+        return result;
     }
 }
