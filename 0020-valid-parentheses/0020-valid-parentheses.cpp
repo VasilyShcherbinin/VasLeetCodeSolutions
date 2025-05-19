27c86ca5d0d631732311622752fbf4758e38237a
Vasily Shcherbinin <vasily.shcherbinin@outlook.com>
2025-05-19 01:23:41 +0100

    Time: 0 ms (100%), Space: 9.1 MB (11.22%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <unordered_set>
+
+unordered_set<char> openingBrackets = { '(', '[', '{' };
+unordered_set<char> closingBrackets = { ')', ']', '}' };
+
+
+class Solution {
+public:
+bool isValid(string s) {
+    stack<char> parenthesis;
+    for (char c : s)
+    {
+        if (openingBrackets.count(c))
+        {
+            parenthesis.push(c);
+        }
+        else if (closingBrackets.count(c))
+        {
+            /* No opening brackets */
+            if (parenthesis.empty())
+            {
+                return false;
+            }
+            else
+            {
+                char ch = parenthesis.top();
+                if (ch == '(' && c == ')' ||
+                    ch == '[' && c == ']' ||
+                    ch == '{' && c == '}')
+                {
+                    parenthesis.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+        }
+    }
+    if (parenthesis.empty())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+};
