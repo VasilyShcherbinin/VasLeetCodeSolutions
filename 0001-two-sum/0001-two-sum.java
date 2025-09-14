fb2af308b57ca20c3a134a15ed513531562acea3
Vasily Shcherbinin <vasily.shcherbinin@outlook.com>
2025-09-14 23:22:27 +0100

    Time: 3 ms (61.67%), Space: 45.4 MB (10.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,19 @@
+import java.util.Hashtable;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Hashtable<Integer, Integer> ht = new Hashtable<>();
         int[] answer = new int[2];
         for (int i = 0; i < nums.length; i++)
         {
-            for (int j = i + 1; j < nums.length; j++)
+            int difference = target - nums[i];
+            if(ht.containsKey(difference))
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    answer[0] = i;
-                    answer[1] = j;
-                }
+                answer[0] = ht.get(difference);
+                answer[1] = i;
+                break;
             }
+            ht.put(nums[i], i);
         }
         return answer;
     }
