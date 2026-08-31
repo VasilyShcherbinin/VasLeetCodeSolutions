abe74dae9ddde37cf8fe594ac41880cdbf9f16bb
Vasily Shcherbinin <vasily.shcherbinin@outlook.com>
2026-08-31 14:50:06 +0100

    Time: 0 ms (100%), Space: 8.8 MB (13.3%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isLongPressedName(std::string name, std::string typed) {
+        int pointerOG{ 0 };
+        int pointerTP{ 0 };
+
+        while (pointerTP < typed.length()) {
+            if (pointerOG < name.length() &&
+                name[pointerOG] == typed[pointerTP]) 
+            {
+                pointerOG++;
+            }
+            else if (pointerTP == 0 ||
+                     typed[pointerTP] != typed[pointerTP - 1]) 
+            {
+                return false;
+            }
+            pointerTP++;
+        }
+
+        return pointerOG == name.length();
+    }
+};
